b47aad1f64f091af7ac129a786b986efd593e172
luozhe73 <1043269439@qq.com>
2019-05-19 16:01:20 +0800

    thymeleaf可运行

--- src/main/java/com/demo/blog/controller/UserController.java
+++ src/main/java/com/demo/blog/controller/UserController.java
@@ -7,12 +7,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    /**
+     * 从 用户存储库 获取用户列表
+     * @return
+     */
+    private List<User> getUserlist() {
+        return userRepository.listUser();
+    }
 
     /**
      * 查询所有用户
@@ -23,7 +33,7 @@ public class UserController {
     @GetMapping
     public ModelAndView list(Model model) {
         model.addAttribute("userList", userRepository.listUser());
-        model.addAttribute("tile", "用户管理");
+        model.addAttribute("title", "用户管理");
         return new ModelAndView("users/list", "userModel", model);
     }
 
@@ -59,6 +69,34 @@ public class UserController {
     public ModelAndView saveOrUpdateUser(User user){
 
         user = userRepository.saveOrUpdateUser(user);
-        return new ModelAndView();
+        return new ModelAndView("redirect:/users");
+    }
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "delete/{id}")
+    public ModelAndView delete(@PathVariable("id") Long id, Model model) {
+        userRepository.deleteUser(id);
+
+        model.addAttribute("userList", getUserlist());
+        model.addAttribute("title", "删除用户");
+        return new ModelAndView("users/list", "userModel", model);
+    }
+
+    /**
+     * 修改用户
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "modify/{id}")
+    public ModelAndView modifyForm(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.getUserById(id);
+
+        model.addAttribute("user", user);
+        model.addAttribute("title", "修改用户");
+        return new ModelAndView("users/form", "userModel", model);
     }
 }
