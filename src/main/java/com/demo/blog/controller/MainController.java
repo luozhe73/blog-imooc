233b8cf0ac20cbcebd9eede964895f2578f730af
luozhe73 <1043269439@qq.com>
2019-05-20 00:57:07 +0800

    spring-security

---
+++ src/main/java/com/demo/blog/controller/MainController.java
@@ -0,0 +1,51 @@
+package com.demo.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 主页控制器
+ */
+@Controller
+public class MainController {
+
+    private static final Long ROLE_USER_AUTHORITY_ID = 2L;
+
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    /**
+     * 获取登录界面
+     * @return
+     */
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        model.addAttribute("errorMsg", "登陆失败，账号或者密码错误！");
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+}
